d7a79b6bc140dcf0bef4c472b8d4cf9d700d4545
Rajeshwari Vanumu <rvan0020@student.monash.edu>
2021-08-24 20:15:06 +1000

    Add project files.

---
+++ Controllers/GetHelpController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class GetHelpController : Controller
+    {
+        // GET: GetHelp
+        public ActionResult GetHelp()
+        {
+            return View();
+        }
+
+        public ActionResult Cannabis()
+        {
+            return View();
+        }
+
+        public ActionResult Ecstasy()
+        {
+            return View();
+
+        }
+
+        public ActionResult Heroin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetHelp()
+        {
+            ViewBag.Message = "This is the get help page";
+
+            return View();
+        }
+
+
+    
+    }
+}
