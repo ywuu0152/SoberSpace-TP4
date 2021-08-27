fe132e60baa567a37299e3d007c079e7b231ba68
ywuu0152 <ywuu0152@student.monash.edu>
2021-08-27 15:36:58 +1000

    IT1.5

---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult Index6()
+        {
+            return View();
+        }
+
+        public ActionResult Index7()
+        {
+            return View();
+        }
+
+        public ActionResult Index8()
+        {
+            return View();
+        }
+
+        public ActionResult Index9()
+        {
+            return View();
+        }
+
+        public ActionResult Index10()
+        {
+            return View();
+        }
+
+        public ActionResult Index11()
+        {
+            return View();
+        }
+
+    }
+}
