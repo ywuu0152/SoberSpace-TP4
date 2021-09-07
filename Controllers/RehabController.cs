9a5aa505301d2bd6cfe23ef8afb690b9a3719cb0
ywuu0152 <ywuu0152@student.monash.edu>
2021-09-07 15:20:34 +1000

    添加项目文件。

---
+++ Controllers/RehabController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class RehabController : Controller
+    {
+        // GET: Rehab
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index_new()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult Index6()
+        {
+            return View();
+        }
+
+        public ActionResult Index7()
+        {
+            return View();
+        }
+
+        public ActionResult Index8()
+        {
+            return View();
+        }
+
+        public ActionResult Index9()
+        {
+            return View();
+        }
+
+        public ActionResult Index10()
+        {
+            return View();
+        }
+
+        public ActionResult Index11()
+        {
+            return View();
+        }
+
+        public ActionResult Index12()
+        {
+            return View();
+        }
+
+        public ActionResult Index13()
+        {
+            return View();
+        }
+
+        public ActionResult Index14()
+        {
+            return View();
+        }
+
+    }
+}
