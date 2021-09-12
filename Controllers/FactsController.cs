e5727ea894b996af10d3b7c220afb5bf03c4eaa3
Rajeshwari Vanumu <rvan0020@student.monash.edu>
2021-09-13 00:32:35 +1000

    Add project files.

---
+++ Controllers/FactsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class FactsController : Controller
+    {
+        // GET: Resource
+        public ActionResult Resource()
+        {
+            return View();
+        }
+
+        public ActionResult Resource2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RehabController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class RehabController : Controller
+    {
+        // GET: Rehab
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+
+    }
+}
